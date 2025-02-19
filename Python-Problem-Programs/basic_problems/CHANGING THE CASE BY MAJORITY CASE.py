0755bb8afa78a882649e3f066b8e26f05d7348c9
Mohammed Shaameer MR <mohammedshaameer4@gmail.com>
2025-02-19 22:05:34 +0530

    Add files via upload

---
+++ Python-Problem-Programs/basic_problems/CHANGING THE CASE BY MAJORITY CASE.py
@@ -0,0 +1,10 @@
+def num(n):
+    x=sum(1 for i in n if i.islower() )
+    Y=sum(1 for i in n if i.isupper() )
+    if x > Y:
+        res=n.lower()
+        print(res)
+    else:
+        print(n.upper())
+
+num("AnbAz")
